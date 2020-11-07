a4a412eb2e2ef46ff8abe4613322e67de6f5518b
KyaTaximo <karina.quispe@taximo.pe>
2020-11-07 09:41:41 -0500

    20201107

---
+++ Otros/Javascript/app.js
@@ -0,0 +1,14 @@
+
+
+function saludar(username){
+    console.log('Hola' + ' ' + username + ' estas aprendiendo ;)');
+} 
+
+saludar('Karina');
+
+
+function sumar(n1,n2){
+    console.log(n1+n2);
+}
+
+sumar(100,200);
